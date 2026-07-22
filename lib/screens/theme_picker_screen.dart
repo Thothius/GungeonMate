94dca45c9c07d59f0ef82e917055fd5a48c4e8eb
GungeonMate Dev <dev@gungeonmate.local>
2026-07-22 21:49:20 +0300

    fix: remove unused shared_preferences import from theme_picker_screen.dart

--- lib/screens/theme_picker_screen.dart
+++ lib/screens/theme_picker_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../services/goop_talk_engine.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
